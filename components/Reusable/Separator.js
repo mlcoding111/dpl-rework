07a1ccd9aa18288ec560508a44f26dfeafe20c31
mlcoding111 <kennycodes@hotmail.com>
2022-03-07 14:59:16 -0500

    Structured and imported home page components

--- components/Reusable/Separator.js
+++ components/Reusable/Separator.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Separator() {
   return (
-    <div>Separator</div>
+    <div>_____________________________________</div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,51 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+
+// Import all components/section
+import Header from "../components/Header/Header";
+import Apropos from "./Apropos";
+import Services from './services'
+import Contact from './contact'
+import Realisations from "./realisations";
+
+import Separator from "../components/Reusable/Separator";
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      {/*  Header */}
+      <Header />
+
+      {/*  Main content of hompage */}
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <section id="services">
+          <Services />
+        </section>
+        <Separator/>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <section id="à-propos">
+          <Apropos />
+        </section>
+        <Separator/>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <section id="contact">
+          <Contact />
+        </section>
+        <Separator/>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        <section id="réalisation">
+          <Realisations />
+        </section>
+        <Separator/>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-
     </div>
-  )
+  );
 }
 
-{/* Default code
+{
+  /* Default code
 <main className={styles.main}>
 <h1 className={styles.title}>
   Welcome to <a href="https://nextjs.org">Next.js!</a>
@@ -91,4 +85,5 @@ export default function Home() {
     </p>
   </a>
 </div>
-</main> */}
+</main> */
+}
