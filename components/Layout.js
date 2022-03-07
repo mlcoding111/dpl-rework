77fd6e0d055886ba864d419d5e5e6c4e3eac808d
mlcoding111 <kennycodes@hotmail.com>
2022-03-07 11:32:58 -0500

    Added basic Layout with NavBar and Footer component. Head title will change depending on the current route for each page

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,20 @@
 import styles from './layout.module.css'
+import NavBar from './NavBar/NavBar'
+import Footer from './Footer/Footer'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 export default function Layout({ children }) {
-    return <div className={styles.container}>{children}</div>
+    const router = useRouter();
+    return (
+        <>
+            <NavBar />
+            <Head>
+                <title>{router.pathname.substring(1)}</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <main>{children}</main>
+            <Footer/>
+        </>
+    )
   }
