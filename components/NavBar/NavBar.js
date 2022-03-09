f969a4aa12649862b1e9aae7eab7b4e4651ac528
mlcoding111 <kennycodes@hotmail.com>
2022-03-09 13:23:58 -0500

    Added mobileNavBar and a toggler

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -7,10 +7,36 @@ import { navLinks } from "../../utils/data";
 
 export default function NavBar() {
   const router = useRouter();
+
+  const [toggled, setToggled] = React.useState(false)
+
+  // Handle mobile nav behaviors
+  const toggleOpen = () => {
+    !toggled ? setToggled(true) : setToggled(false) // Handle className
+    let cc = document.querySelector(`.${navBar.mobileNav} ul`)
+    console.log(cc)
+
+    if(toggled){
+      setToggled(false)
+      cc.style.left = "-100%"
+    }else{
+      setToggled(true)
+      cc.style.left = "0"
+    }
+
+  }
   return (
     <div className={navBar.wrapper}>
     <nav className={navBar.nav}>
+      
       <ul>
+        {/* Mobile Nav Toggler */}
+        <div className={`${navBar.toggler}  ${toggled ? navBar.open : ''}`} onClick={toggleOpen}>
+              <span></span>
+              <span></span>
+              <span></span>
+        </div>
+        {/* Desktop Nav */}
         {navLinks.map((link, index) => {
           return (
             <Link href={link.path} key={index} activeClassName="active">
@@ -23,7 +49,26 @@ export default function NavBar() {
           );
         })}
       </ul>
+
     </nav>
+    
+     {/* Mobile Nav */}
+    <nav className={navBar.mobileNav}>
+      <ul>
+        {navLinks.map((link, index) => {
+          return (
+            <Link href={link.path} key={index} activeClassName="active">
+              <li >
+                <a href="#" className={router.pathname == link.path ? navBar.active : ""} onClick={toggleOpen}> {/* Toggle active class when router path change */ }
+                  {link.name}
+                </a>
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
+
     </div>
   );
 }
