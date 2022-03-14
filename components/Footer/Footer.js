9cd0ff2330e6061e92dde6f0cc417f4f7619b3d8
mlcoding111 <kennycodes@hotmail.com>
2022-03-14 13:45:47 -0400

    Footer rework

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,41 +1,77 @@
 import React from "react";
 import styles from "./footer.module.css";
-import Link from "next/link";
-import {Container, Row, Col} from 'react-bootstrap'
-import { navLinks } from "../../utils/data";
-import { useRouter } from 'next/router';
-import Image from 'next/image'
-import Logo from '../../public/images/brand.svg'
+import { Container, Row, Col } from "react-bootstrap";
+import { useRouter } from "next/router";
+import {IoHomeSharp, IoMailSharp} from 'react-icons/io5'
+import {MdPhone} from 'react-icons/md'
 
 export default function Footer() {
   const router = useRouter();
   return (
     <footer className={styles.footer}>
-      <Container fluid className="d-flex flex-column h-100">
-        <Row className="h-100">
-          <Col md="12" className="d-flex text-center justify-content-center flex-column pt-5">
-            <nav>
-              <ul className="d-flex text-white">
-              {navLinks.map((link, index) => {
-          return (
-            <Link href={link.path} key={index} activeClassName="active">
-              <li >
-                <a href="#" > {/* Toggle active class when router path change */ }
-                  {link.name}
-                </a>
-              </li>
-            </Link>
-          );
-        })}
-          </ul>
-          
-            </nav>
+      <Container className="text-center text-md-left">
+        <Row className="text-center text-md-left">
+          <Col md="3" lg="3" xl="4" className="mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold">
+            Décontamination Précision Lacroix
+            </h5>
+            {/* <p className="long-text">Salut oti</p> */}
           </Col>
+          <Col md="5" className="mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold">Services<hr/></h5>
+            <ul>
+              <li>- Démolition</li>
+              <li>- Décontamination</li>
+              <li>- Wetblast</li>
+            </ul>
+          </Col>
+          <Col md="4" lg="3" xl="3" className="mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold">Contact<hr/></h5>
+            <ul>
+              <li><IoHomeSharp/> <span>Québec, Canada</span></li>
+            </ul>
+            <ul>
+              <li><IoMailSharp/> <span>Dplacroix@outlook.com</span></li>
+            </ul>
+            <ul>
+              <li><MdPhone/> <span>819-993-8686</span></li>
+            </ul>
+          </Col>
+          <hr></hr>
         </Row>
-        
-          {/* <Image src={Logo} height="100"></Image> */}
-          <Row className="text-center">
-          <p id="copy-right">Copyright 2021 DPL - All Rights Reserverd.</p>      
+
+        <Row clasName="align-items-center">
+          <Col md="7" lg="8">
+          <p class={styles.copyRight}>Copyright 2022 DPL - All Rights Reserverd.</p>    
+          </Col>
+
+          { /* Social */}
+          {/* <Col md="5" lg="4">
+            <div className="text-center text-md-right">
+              <ul className="list-unstyled list-inline">
+                <li className="list-inline-item">
+                    <a href="#" className="btn-floating btn-sm text-white">
+
+                    </a>
+                </li>
+                <li className="list-inline-item">
+                    <a href="#" className="btn-floating btn-sm text-white">
+                      
+                    </a>
+                </li>
+                <li className="list-inline-item">
+                    <a href="#" className="btn-floating btn-sm text-white">
+                      
+                    </a>
+                </li>
+                <li className="list-inline-item">
+                    <a href="#" className="btn-floating btn-sm text-white">
+                      
+                    </a>
+                </li>
+              </ul>
+            </div>
+          </Col> */}
         </Row>
       </Container>
     </footer>
